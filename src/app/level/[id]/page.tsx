dede43e9f25d4494b9629c1f33a58b282bd2e85e
MarlinDiary <hi@huizha.com>
2025-08-04 17:18:08 +1200

    add a resizable panel component

--- src/app/level/[id]/page.tsx
+++ src/app/level/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { notFound } from 'next/navigation'
+import { ResizablePanelGroup, ResizablePanel, ResizableHandle } from '@/components/ui/resizable'
 
 interface LevelPageProps {
   params: Promise<{ id: string }>
@@ -13,8 +14,43 @@ export default async function LevelPage({ params }: LevelPageProps) {
   }
 
   return (
-    <div>
-      Level {id}
+    <div className="h-screen w-full" style={{ backgroundColor: '#f0f0f0' }}>
+      <ResizablePanelGroup direction="horizontal">
+        {/* First column with two panels */}
+        <ResizablePanel defaultSize={33}>
+          <ResizablePanelGroup direction="vertical">
+            <ResizablePanel defaultSize={50}>
+              <div className="p-4 h-full bg-gray-50">
+                Panel 1 - Level {id}
+              </div>
+            </ResizablePanel>
+            <ResizableHandle className="opacity-0" />
+            <ResizablePanel defaultSize={50}>
+              <div className="p-4 h-full bg-gray-100">
+                Panel 2 - Level {id}
+              </div>
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </ResizablePanel>
+        
+        <ResizableHandle className="opacity-0" />
+        
+        {/* Second column */}
+        <ResizablePanel defaultSize={34}>
+          <div className="p-4 h-full bg-blue-50">
+            Panel 3 - Level {id}
+          </div>
+        </ResizablePanel>
+        
+        <ResizableHandle className="opacity-0" />
+        
+        {/* Third column */}
+        <ResizablePanel defaultSize={33}>
+          <div className="p-4 h-full bg-green-50">
+            Panel 4 - Level {id}
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   )
 }
